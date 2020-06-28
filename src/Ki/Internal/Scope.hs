d7f631be322c9028909b895a5d9a1c4b9109952b
Mitchell Rosen <mitchellwrosen@gmail.com>
2020-06-28 09:52:25 -0400

    internal scope (no implicit context)

--- src/Ki/Internal/Scope.hs
+++ src/Ki/Internal/Scope.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE TypeApplications #-}
-
 module Ki.Internal.Scope
   ( Scope,
     async,
@@ -11,113 +8,19 @@ module Ki.Internal.Scope
   )
 where
 
-import Control.Exception (AsyncException (ThreadKilled), Exception (fromException), pattern ErrorCall)
-import qualified Data.Monoid as Monoid
-import qualified Data.Set as Set
 import Ki.Internal.Concurrency
-import Ki.Internal.Context (CancelToken (..), Cancelled (..), Context)
-import qualified Ki.Internal.Context as Context
-import qualified Ki.Internal.Context.Internal
-import Ki.Internal.Prelude
-import Ki.Internal.Thread (AsyncThreadFailed (..), Thread (Thread))
-import qualified Ki.Internal.Thread as Thread
+import Ki.Internal.Context (Context)
+import Ki.Internal.Scope.Internal (Scope, cancel, wait)
+import qualified Ki.Internal.Scope.Internal as Internal
+import Ki.Internal.Thread (Thread)
 
--- | A __scope__ delimits the lifetime of all __threads__ forked within it. A __thread__ cannot outlive its __scope__.
---
--- When a __scope__ is /closed/, all remaining __threads__ forked within it are killed.
---
--- The basic usage of a __scope__ is as follows.
---
--- @
--- 'scoped' \\scope -> do
---   'fork' scope worker1
---   'fork' scope worker2
---   'wait' scope
--- @
---
--- A __scope__ can be passed into functions or shared amongst __threads__, but this is generally not advised, as it
--- takes the "structure" out of "structured concurrency".
-data Scope = Scope
-  { context :: Ki.Internal.Context.Internal.Context,
-    -- | Whether this scope is closed
-    -- Invariant: if closed, no threads are starting
-    closedVar :: TVar Bool,
-    -- | The set of threads that are currently running
-    runningVar :: TVar (Set ThreadId),
-    -- | The number of threads that are guaranteed to be just about to start. If this number is non-zero, and that's
-    -- problematic (e.g. if we're about to cancel this scope), we always respect it and wait for it to drop to zero
-    -- before proceeding.
-    startingVar :: TVar Int
-  }
-
-async :: forall a. Scope -> (Context => (forall x. IO x -> IO x) -> IO a) -> IO (Thread a)
-async scope action = do
-  resultVar <- newEmptyTMVarIO
-  childThreadId <-
-    _fork scope action \result ->
-      atomically (putTMVar resultVar result)
-  pure (Thread childThreadId (readTMVar resultVar))
-
--- | /Cancel/ all __contexts__ derived from a __scope__.
-cancel :: Scope -> IO ()
-cancel Scope {context} = do
-  n <- uniqueInt
-  atomically (Context.cancel context (CancelToken n))
-
--- | Close a scope, kill all of the running threads, and return the first
--- async exception delivered to us while doing so, if any.
---
--- Preconditions:
---   * The set of threads doesn't include us
---   * We're uninterruptibly masked
-close :: Scope -> IO (Maybe SomeException)
-close scope@Scope {closedVar, runningVar} = do
-  threads <-
-    atomically do
-      blockUntilNoneStarting scope
-      writeTVar closedVar True
-      readTVar runningVar
-  exception <- killThreads (Set.toList threads)
-  atomically (blockUntilNoneRunning scope)
-  pure (Monoid.getFirst exception)
+async :: Scope -> (Context => (forall x. IO x -> IO x) -> IO a) -> IO (Thread a)
+async scope action =
+  Internal.async scope \restore -> withContext scope (action restore)
 
 fork :: Scope -> (Context => (forall x. IO x -> IO x) -> IO ()) -> IO ()
-fork scope@Scope {context} action = do
-  parentThreadId <- myThreadId
-  _ <-
-    _fork scope action \result ->
-      whenLeft result \exception ->
-        whenM
-          (shouldPropagateException context exception)
-          (throwTo parentThreadId (AsyncThreadFailed exception))
-  pure ()
-
-_fork :: Scope -> (Context => (forall x. IO x -> IO x) -> IO a) -> (Either SomeException a -> IO ()) -> IO ThreadId
-_fork scope action k =
-  uninterruptibleMask \restore -> do
-    atomically (incrementStarting scope)
-    childThreadId <-
-      forkIO do
-        result <- try (inScope scope (action restore))
-        k result
-        childThreadId <- myThreadId
-        atomically (deleteRunning scope childThreadId)
-    atomically do
-      decrementStarting scope
-      insertRunning scope childThreadId
-    pure childThreadId
-
-inScope :: Scope -> (Context => IO a) -> IO a
-inScope Scope {context} action =
-  let ?context = context in action
-
-new :: Context => IO Scope
-new = do
-  context <- atomically (Context.derive ?context)
-  closedVar <- newTVarIO False
-  runningVar <- newTVarIO Set.empty
-  startingVar <- newTVarIO 0
-  pure Scope {context, closedVar, runningVar, startingVar}
+fork scope action =
+  Internal.fork scope \restore -> withContext scope (action restore)
 
 -- | Perform an action with a new __scope__, then /close/ the __scope__.
 --
@@ -134,86 +37,9 @@ new = do
 --   'wait' scope
 -- @
 scoped :: Context => (Context => Scope -> IO a) -> IO a
-scoped f = do
-  scope <- new
-  uninterruptibleMask \restore -> do
-    result <- try (restore (inScope scope (f scope)))
-    closeScopeException <- close scope
-    case result of
-      -- If the callback failed, we don't care if we were thrown an async exception while closing the scope
-      Left exception -> throwIO (Thread.unwrapAsyncThreadFailed exception)
-      -- Otherwise, throw that exception (if it exists)
-      Right value -> do
-        for_ @Maybe closeScopeException (throwIO . Thread.unwrapAsyncThreadFailed)
-        pure value
+scoped action =
+  Internal.scoped ?context \scope -> withContext scope (action scope)
 
-wait :: Scope -> STM ()
-wait scope = do
-  blockUntilNoneRunning scope
-  blockUntilNoneStarting scope
-
---------------------------------------------------------------------------------
--- Scope helpers
-
-incrementStarting :: Scope -> STM ()
-incrementStarting Scope {closedVar, startingVar} =
-  readTVar closedVar >>= \case
-    False -> modifyTVar' startingVar (+ 1)
-    True -> throwSTM (ErrorCall "ki: scope closed")
-
-decrementStarting :: Scope -> STM ()
-decrementStarting Scope {startingVar} =
-  modifyTVar' startingVar (subtract 1)
-
-insertRunning :: Scope -> ThreadId -> STM ()
-insertRunning Scope {runningVar} threadId =
-  modifyTVar' runningVar (Set.insert threadId)
-
-deleteRunning :: Scope -> ThreadId -> STM ()
-deleteRunning Scope {runningVar} threadId = do
-  running <- readTVar runningVar
-  case Set.splitMember threadId running of
-    (xs, True, ys) -> writeTVar runningVar $! Set.union xs ys
-    _ -> retry
-
-blockUntilNoneRunning :: Scope -> STM ()
-blockUntilNoneRunning Scope {runningVar} =
-  blockUntilTVar runningVar Set.null
-
-blockUntilNoneStarting :: Scope -> STM ()
-blockUntilNoneStarting Scope {startingVar} =
-  blockUntilTVar startingVar (== 0)
-
---------------------------------------------------------------------------------
--- Misc. utils
-
-killThreads :: [ThreadId] -> IO (Monoid.First SomeException)
-killThreads =
-  loop mempty
-  where
-    loop :: Monoid.First SomeException -> [ThreadId] -> IO (Monoid.First SomeException)
-    loop acc = \case
-      [] -> pure acc
-      threadId : threadIds ->
-        -- We unmask because we don't want to deadlock with a thread
-        -- that is concurrently trying to throw an exception to us with
-        -- exceptions masked.
-        try (unsafeUnmask (throwTo threadId ThreadKilled)) >>= \case
-          -- don't drop thread we didn't kill
-          Left exception -> loop (acc <> pure exception) (threadId : threadIds)
-          Right () -> loop acc threadIds
-
-shouldPropagateException :: Ki.Internal.Context.Internal.Context -> SomeException -> IO Bool
-shouldPropagateException context exception =
-  case fromException exception of
-    Just ThreadKilled -> pure False
-    Just _ -> pure True
-    Nothing ->
-      case fromException exception of
-        Just (Cancelled_ token) -> atomically (((/= token) <$> Context.cancelled context) <|> pure True)
-        Nothing -> pure True
-
-blockUntilTVar :: TVar a -> (a -> Bool) -> STM ()
-blockUntilTVar var f = do
-  value <- readTVar var
-  unless (f value) retry
+withContext :: Scope -> (Context => IO a) -> IO a
+withContext scope action =
+  Internal.withContext scope \context -> let ?context = context in action
---
+++ src/Ki/Internal/Scope/Internal.hs
@@ -0,0 +1,205 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Ki.Internal.Scope.Internal
+  ( Scope,
+    async,
+    cancel,
+    fork,
+    scoped,
+    wait,
+    withContext,
+  )
+where
+
+import Control.Exception (AsyncException (ThreadKilled), Exception (fromException), pattern ErrorCall)
+import qualified Data.Monoid as Monoid
+import qualified Data.Set as Set
+import Ki.Internal.Concurrency
+import Ki.Internal.Context.Internal (CancelToken (..), Cancelled (..), Context)
+import qualified Ki.Internal.Context.Internal as Context
+import Ki.Internal.Prelude
+import Ki.Internal.Thread (AsyncThreadFailed (..), Thread (Thread))
+import qualified Ki.Internal.Thread as Thread
+
+-- | A __scope__ delimits the lifetime of all __threads__ forked within it. A __thread__ cannot outlive its __scope__.
+--
+-- When a __scope__ is /closed/, all remaining __threads__ forked within it are killed.
+--
+-- The basic usage of a __scope__ is as follows.
+--
+-- @
+-- 'scoped' \\scope -> do
+--   'fork' scope worker1
+--   'fork' scope worker2
+--   'wait' scope
+-- @
+--
+-- A __scope__ can be passed into functions or shared amongst __threads__, but this is generally not advised, as it
+-- takes the "structure" out of "structured concurrency".
+data Scope = Scope
+  { context :: Context,
+    -- | Whether this scope is closed
+    -- Invariant: if closed, no threads are starting
+    closedVar :: TVar Bool,
+    -- | The set of threads that are currently running
+    runningVar :: TVar (Set ThreadId),
+    -- | The number of threads that are guaranteed to be just about to start. If this number is non-zero, and that's
+    -- problematic (e.g. if we're about to cancel this scope), we always respect it and wait for it to drop to zero
+    -- before proceeding.
+    startingVar :: TVar Int
+  }
+
+async :: forall a. Scope -> ((forall x. IO x -> IO x) -> IO a) -> IO (Thread a)
+async scope action = do
+  resultVar <- newEmptyTMVarIO
+  childThreadId <-
+    _fork scope action \result ->
+      atomically (putTMVar resultVar result)
+  pure (Thread childThreadId (readTMVar resultVar))
+
+-- | /Cancel/ all __contexts__ derived from a __scope__.
+cancel :: Scope -> IO ()
+cancel Scope {context} = do
+  n <- uniqueInt
+  atomically (Context.cancel context (CancelToken n))
+
+-- | Close a scope, kill all of the running threads, and return the first
+-- async exception delivered to us while doing so, if any.
+--
+-- Preconditions:
+--   * The set of threads doesn't include us
+--   * We're uninterruptibly masked
+close :: Scope -> IO (Maybe SomeException)
+close scope@Scope {closedVar, runningVar} = do
+  threads <-
+    atomically do
+      blockUntilNoneStarting scope
+      writeTVar closedVar True
+      readTVar runningVar
+  exception <- killThreads (Set.toList threads)
+  atomically (blockUntilNoneRunning scope)
+  pure (Monoid.getFirst exception)
+
+fork :: Scope -> ((forall x. IO x -> IO x) -> IO ()) -> IO ()
+fork scope@Scope {context} action = do
+  parentThreadId <- myThreadId
+  _ <-
+    _fork scope action \result ->
+      whenLeft result \exception ->
+        whenM
+          (shouldPropagateException context exception)
+          (throwTo parentThreadId (AsyncThreadFailed exception))
+  pure ()
+
+_fork :: Scope -> ((forall x. IO x -> IO x) -> IO a) -> (Either SomeException a -> IO ()) -> IO ThreadId
+_fork scope action k =
+  uninterruptibleMask \restore -> do
+    atomically (incrementStarting scope)
+    childThreadId <-
+      forkIO do
+        result <- try (action restore)
+        k result
+        childThreadId <- myThreadId
+        atomically (deleteRunning scope childThreadId)
+    atomically do
+      decrementStarting scope
+      insertRunning scope childThreadId
+    pure childThreadId
+
+new :: Context -> IO Scope
+new parentContext = do
+  context <- atomically (Context.derive parentContext)
+  closedVar <- newTVarIO False
+  runningVar <- newTVarIO Set.empty
+  startingVar <- newTVarIO 0
+  pure Scope {context, closedVar, runningVar, startingVar}
+
+scoped :: Context -> (Scope -> IO a) -> IO a
+scoped context f = do
+  scope <- new context
+  uninterruptibleMask \restore -> do
+    result <- try (restore (f scope))
+    closeScopeException <- close scope
+    case result of
+      -- If the callback failed, we don't care if we were thrown an async exception while closing the scope
+      Left exception -> throwIO (Thread.unwrapAsyncThreadFailed exception)
+      -- Otherwise, throw that exception (if it exists)
+      Right value -> do
+        for_ @Maybe closeScopeException (throwIO . Thread.unwrapAsyncThreadFailed)
+        pure value
+
+wait :: Scope -> STM ()
+wait scope = do
+  blockUntilNoneRunning scope
+  blockUntilNoneStarting scope
+
+withContext :: Scope -> (Context -> a) -> a
+withContext Scope {context} action =
+  action context
+
+--------------------------------------------------------------------------------
+-- Scope helpers
+
+incrementStarting :: Scope -> STM ()
+incrementStarting Scope {closedVar, startingVar} =
+  readTVar closedVar >>= \case
+    False -> modifyTVar' startingVar (+ 1)
+    True -> throwSTM (ErrorCall "ki: scope closed")
+
+decrementStarting :: Scope -> STM ()
+decrementStarting Scope {startingVar} =
+  modifyTVar' startingVar (subtract 1)
+
+insertRunning :: Scope -> ThreadId -> STM ()
+insertRunning Scope {runningVar} threadId =
+  modifyTVar' runningVar (Set.insert threadId)
+
+deleteRunning :: Scope -> ThreadId -> STM ()
+deleteRunning Scope {runningVar} threadId = do
+  running <- readTVar runningVar
+  case Set.splitMember threadId running of
+    (xs, True, ys) -> writeTVar runningVar $! Set.union xs ys
+    _ -> retry
+
+blockUntilNoneRunning :: Scope -> STM ()
+blockUntilNoneRunning Scope {runningVar} =
+  blockUntilTVar runningVar Set.null
+
+blockUntilNoneStarting :: Scope -> STM ()
+blockUntilNoneStarting Scope {startingVar} =
+  blockUntilTVar startingVar (== 0)
+
+--------------------------------------------------------------------------------
+-- Misc. utils
+
+killThreads :: [ThreadId] -> IO (Monoid.First SomeException)
+killThreads =
+  loop mempty
+  where
+    loop :: Monoid.First SomeException -> [ThreadId] -> IO (Monoid.First SomeException)
+    loop acc = \case
+      [] -> pure acc
+      threadId : threadIds ->
+        -- We unmask because we don't want to deadlock with a thread
+        -- that is concurrently trying to throw an exception to us with
+        -- exceptions masked.
+        try (unsafeUnmask (throwTo threadId ThreadKilled)) >>= \case
+          -- don't drop thread we didn't kill
+          Left exception -> loop (acc <> pure exception) (threadId : threadIds)
+          Right () -> loop acc threadIds
+
+shouldPropagateException :: Ki.Internal.Context.Internal.Context -> SomeException -> IO Bool
+shouldPropagateException context exception =
+  case fromException exception of
+    Just ThreadKilled -> pure False
+    Just _ -> pure True
+    Nothing ->
+      case fromException exception of
+        Just (Cancelled_ token) -> atomically (((/= token) <$> Context.cancelled context) <|> pure True)
+        Nothing -> pure True
+
+blockUntilTVar :: TVar a -> (a -> Bool) -> STM ()
+blockUntilTVar var f = do
+  value <- readTVar var
+  unless (f value) retry
