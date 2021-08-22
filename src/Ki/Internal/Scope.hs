7dc47510e0d4a3cf32e8df446865c8129c64b6a1
Mitchell Rosen <mitchellwrosen@gmail.com>
2021-08-22 19:44:21 -0400

    inline Children type

--- src/Ki/Internal/Scope.hs
+++ src/Ki/Internal/Scope.hs
@@ -1,6 +1,5 @@
 module Ki.Internal.Scope
   ( Scope (..),
-    Children (..),
     scopeFork,
     scopeScoped,
     scopeScopedIO,
@@ -28,31 +27,19 @@ import Ki.Internal.Timeout
 
 -- | A __scope__ delimits the lifetime of all __threads__ created within it.
 data Scope = Scope
-  { -- | The set of threads that are currently running.
-    scope'runningVar :: {-# UNPACK #-} !(TVar Children),
-    -- | The number of threads that are *guaranteed* to be about to start, in the sense that only the GHC scheduler can
-    -- continue to delay; no async exception can strike here and prevent one of these threads from starting.
-    --
-    -- If this number is non-zero, and that's problematic (e.g. because we're trying to cancel this scope), we always
-    -- respect it and wait for it to drop to zero before proceeding.
+  { -- | The set of child threads that are currently running, each keyed by a monotonically increasing int.
+    childrenVar :: {-# UNPACK #-} !(TVar (IntMap ThreadId)),
+    -- | The key to use for the next child thread.
+    nextChildIdVar :: {-# UNPACK #-} !(TVar Int),
+    -- | The number of child threads that are guaranteed to be about to start, in the sense that only the GHC scheduler
+    -- can continue to delay; no async exception can strike here and prevent one of these threads from starting.
     --
     -- Sentinel value: -1 means the scope is closed.
     scope'startingVar :: {-# UNPACK #-} !(TVar Int)
   }
 
--- | Children, keyed by a monotonically increasing integer, along with the next integer to use. This allows us to kill
--- child threads in the order they were created.
---
--- It would probably be too slow to throw an async exception to a child, then synchronously wait for the child to
--- terminate before throwing an async exception to the next child, so instead we just deliver the exceptions in order,
--- which is still a useful property to have.
-data Children = Children
-  { children'children :: !(IntMap ThreadId),
-    children'nextId :: {-# UNPACK #-} !Int
-  }
-
 scopeFork :: Scope -> ((forall x. IO x -> IO x) -> IO a) -> (Either SomeException a -> IO ()) -> IO ThreadId
-scopeFork Scope {scope'runningVar, scope'startingVar} action k =
+scopeFork Scope {childrenVar, nextChildIdVar, scope'startingVar} action k =
   uninterruptibleMask \restore -> do
     -- Record the thread as being about to start, and grab an id for it
     childId <-
@@ -61,9 +48,8 @@ scopeFork Scope {scope'runningVar, scope'startingVar} action k =
         if starting == -1
           then throwSTM (ErrorCall "ki: scope closed")
           else do
-            children <- readTVar scope'runningVar
-            let childId = children'nextId children
-            writeTVar scope'runningVar $! children {children'nextId = childId + 1}
+            childId <- readTVar nextChildIdVar
+            writeTVar nextChildIdVar $! childId + 1
             writeTVar scope'startingVar $! starting + 1
             pure childId
 
@@ -76,16 +62,15 @@ scopeFork Scope {scope'runningVar, scope'startingVar} action k =
         -- condition) - we wouldn't want to delete *nothing*, *then* insert from the parent thread. So just retry until
         -- the parent has recorded us as having started.
         atomically do
-          children <- readTVar scope'runningVar
-          case IntMap.alterF (maybe Nothing (const (Just Nothing))) childId (children'children children) of
+          children <- readTVar childrenVar
+          case IntMap.alterF (maybe Nothing (const (Just Nothing))) childId children of
             Nothing -> retry
-            Just running -> writeTVar scope'runningVar $! children {children'children = running}
+            Just running -> writeTVar childrenVar running
 
     -- Record the thread as having started
     atomically do
       modifyTVar' scope'startingVar \n -> n -1
-      modifyTVar' scope'runningVar \children ->
-        children {children'children = IntMap.insert childId childThreadId (children'children children)}
+      modifyTVar' childrenVar (IntMap.insert childId childThreadId)
 
     pure childThreadId
 
@@ -96,11 +81,14 @@ scopeScoped action =
 
 scopeScopedIO :: (Scope -> IO a) -> IO a
 scopeScopedIO f = do
+  childrenVar <- newTVarIO IntMap.empty
+  nextChildIdVar <- newTVarIO 0
+  scope'startingVar <- newTVarIO 0
+  let scope = Scope {childrenVar, nextChildIdVar, scope'startingVar}
+
   uninterruptibleMask \restore -> do
-    scope'runningVar <- newTVarIO (Children IntMap.empty 0)
-    scope'startingVar <- newTVarIO 0
-    let scope = Scope {scope'runningVar, scope'startingVar}
     result <- try (restore (f scope))
+
     children <-
       atomically do
         -- Block until we haven't committed to starting any threads. Without this, we may create a thread concurrently
@@ -109,19 +97,14 @@ scopeScopedIO f = do
         -- Write the sentinel value indicating that this scope is closed, and it is an error to try to create a thread
         -- within it.
         writeTVar scope'startingVar (-1)
-        -- Remove our parent scope's reference to us. This is necessary for long-lived parent scopes that create and
-        -- destroy many child scopes dynamically - we shouldn't leak memory by needlessly growing the parent's list of
-        -- children forever. Plus, when/if the parent is cancelled, we'd like as small a list of children as possible to
-        -- propagate the cancellation to.
-        -- removeContextFromParent
         -- Return the list of currently-running children to kill. Some of them may have *just* started (e.g. if we
         -- initially retried in 'blockUntilNoneStarting' above). That's fine - kill them all!
-        readTVar scope'runningVar
+        readTVar childrenVar
 
     -- Deliver an async exception to every child. While doing so, we may get hit by an async exception ourselves, which
     -- we don't want to just ignore. (Actually, we may have been hit by an arbitrary number of async exceptions,
     -- but it's unclear what we would do with such a list, so we only remember the first one, and ignore the others).
-    exceptionReceivedWhileKillingChildren <- killThreads (IntMap.elems (children'children children))
+    exceptionReceivedWhileKillingChildren <- killThreads (IntMap.elems children)
 
     -- Block until all children have terminated; this relies on children respecting the async exception, which they
     -- must, for correctness. Otherwise, a thread could indeed outlive the scope in which it's created, which is
@@ -170,8 +153,8 @@ scopeWaitSTM scope = do
 -- Scope helpers
 
 blockUntilNoneRunning :: Scope -> STM ()
-blockUntilNoneRunning Scope {scope'runningVar} =
-  blockUntilTVar scope'runningVar \(Children children _) -> IntMap.null children
+blockUntilNoneRunning Scope {childrenVar} =
+  blockUntilTVar childrenVar IntMap.null
 
 blockUntilNoneStarting :: Scope -> STM ()
 blockUntilNoneStarting Scope {scope'startingVar} =
